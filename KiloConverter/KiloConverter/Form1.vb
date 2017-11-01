b702e74d747ec88487938e60b1a189be8c15a721
dl137273 <33095988+dl137273@users.noreply.github.com>
2017-11-01 10:11:06 -0500

    Updated Information

--- KiloConverter/KiloConverter/Form1.vb
+++ KiloConverter/KiloConverter/Form1.vb
@@ -1,3 +1,31 @@
-﻿Public Class Form1
+﻿Public Class KiloConverter
 
+
+    Private Sub btnInches_Click(sender As Object, e As EventArgs) Handles btnInches.Click
+        lblMessage.Text = "1 Kilometer = 39,370 inches"
+
+    End Sub 
+    Private Sub btnFeet_Click(sender As Object, e As EventArgs) Handles btnFeet.Click
+        lblMessage.Text = "1 Kilometer = 3,281 feet"
+
+    End Sub
+
+    Private Sub btnYards_Click(sender As Object, e As EventArgs) Handles btnYards.Click
+        lblMessage.Text = "1 Kilometer = 1,093.6 yards"
+
+    End Sub
+
+    Private Sub btnMiles_Click(sender As Object, e As EventArgs) Handles btnMiles.Click
+        lblMessage.Text = "1 Kilometer = .06214 miles"
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub KiloConverter_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
