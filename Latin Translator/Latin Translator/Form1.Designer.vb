82c6a3a5b55f93497a5d539b97561f15bf0b9a54
dl137273 <33095988+dl137273@users.noreply.github.com>
2017-11-03 10:31:54 -0500

    Created Latin Translator

---
+++ Latin Translator/Latin Translator/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnSinister = New System.Windows.Forms.Button()
+        Me.btnDexter = New System.Windows.Forms.Button()
+        Me.btnMedium = New System.Windows.Forms.Button()
+        Me.lblLeft = New System.Windows.Forms.Label()
+        Me.lblRight = New System.Windows.Forms.Label()
+        Me.lblCenter = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(40, 63)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 19)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Latin"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(202, 63)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 19)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "English"
+        '
+        'btnSinister
+        '
+        Me.btnSinister.Location = New System.Drawing.Point(44, 91)
+        Me.btnSinister.Name = "btnSinister"
+        Me.btnSinister.Size = New System.Drawing.Size(75, 23)
+        Me.btnSinister.TabIndex = 2
+        Me.btnSinister.Text = "sinister"
+        Me.btnSinister.UseVisualStyleBackColor = True
+        '
+        'btnDexter
+        '
+        Me.btnDexter.Location = New System.Drawing.Point(44, 133)
+        Me.btnDexter.Name = "btnDexter"
+        Me.btnDexter.Size = New System.Drawing.Size(75, 23)
+        Me.btnDexter.TabIndex = 3
+        Me.btnDexter.Text = "dexter"
+        Me.btnDexter.UseVisualStyleBackColor = True
+        '
+        'btnMedium
+        '
+        Me.btnMedium.Location = New System.Drawing.Point(44, 175)
+        Me.btnMedium.Name = "btnMedium"
+        Me.btnMedium.Size = New System.Drawing.Size(75, 23)
+        Me.btnMedium.TabIndex = 4
+        Me.btnMedium.Text = "medium"
+        Me.btnMedium.UseVisualStyleBackColor = True
+        '
+        'lblLeft
+        '
+        Me.lblLeft.AutoSize = True
+        Me.lblLeft.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.lblLeft.Location = New System.Drawing.Point(203, 96)
+        Me.lblLeft.Name = "lblLeft"
+        Me.lblLeft.Size = New System.Drawing.Size(21, 13)
+        Me.lblLeft.TabIndex = 5
+        Me.lblLeft.Text = "left"
+        Me.lblLeft.Visible = False
+        '
+        'lblRight
+        '
+        Me.lblRight.AutoSize = True
+        Me.lblRight.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.lblRight.Location = New System.Drawing.Point(203, 138)
+        Me.lblRight.Name = "lblRight"
+        Me.lblRight.Size = New System.Drawing.Size(27, 13)
+        Me.lblRight.TabIndex = 6
+        Me.lblRight.Text = "right"
+        Me.lblRight.Visible = False
+        '
+        'lblCenter
+        '
+        Me.lblCenter.AutoSize = True
+        Me.lblCenter.Location = New System.Drawing.Point(203, 180)
+        Me.lblCenter.Name = "lblCenter"
+        Me.lblCenter.Size = New System.Drawing.Size(37, 13)
+        Me.lblCenter.TabIndex = 7
+        Me.lblCenter.Text = "center"
+        Me.lblCenter.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(315, 262)
+        Me.Controls.Add(Me.lblCenter)
+        Me.Controls.Add(Me.lblRight)
+        Me.Controls.Add(Me.lblLeft)
+        Me.Controls.Add(Me.btnMedium)
+        Me.Controls.Add(Me.btnDexter)
+        Me.Controls.Add(Me.btnSinister)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnSinister As System.Windows.Forms.Button
+    Friend WithEvents btnDexter As System.Windows.Forms.Button
+    Friend WithEvents btnMedium As System.Windows.Forms.Button
+    Friend WithEvents lblLeft As System.Windows.Forms.Label
+    Friend WithEvents lblRight As System.Windows.Forms.Label
+    Friend WithEvents lblCenter As System.Windows.Forms.Label
+
+End Class
